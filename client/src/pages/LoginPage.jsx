0a8b71a95294557fa8154cf08163d6fd8a74bded
Teodor <teodorpatov05@pmgkk.com>
2024-07-14 18:04:18 +0300

    Initialize home page and login page

---
+++ client/src/pages/LoginPage.jsx
@@ -0,0 +1,13 @@
+export default function LoginPage(){
+    return (
+        <div className="mt-4">
+            <h1 className="text-4xl text-center mb-4">Login</h1>
+            <form className="max-w-md mx-auto">
+                <input type="email" placeholder="you@email.com" />
+                <input type="password" placeholder="password" />
+                <button className="primary">Login</button>
+            </form>
+        </div>
+    );
+
+}
