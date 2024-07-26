2b92bba12e54b3b9a18992c521d7e1728f35cb5b
Teodor <teodorpatov05@pmgkk.com>
2024-07-26 17:50:55 +0300

    Added regiser functionality

--- api/index.js
+++ api/index.js
@@ -1,14 +1,20 @@
-import express from 'express';
-import cors from 'cors';
-import mongoose from 'mongoose';
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const User = require('./models/User.js')
+const bcrypt = require('bcrypt')
 require('dotenv').config();
 const app = express();
+
+const bcryptSalt = bcrypt.genSaltSync(10);
 
 app.use(express.json());
 app.use(cors({
     credentials: true, 
-    origin: 'http://127.0.0.1:5173'
+    origin: 'http://localhost:5173'
 }));
+
+console.log(process.env.MONGO_URL)
 
 mongoose.connect(process.env.MONGO_URL);
 
@@ -16,9 +22,15 @@ app.get('/test', (req, res) => {
     res.json('test ok');
 });
 
-app.post('/register', (req, res) => {
+app.post('/register', async (req, res) => {
     const {name, email, password} = req.body;
-    res.json({name, email, password});
+    const userDoc = await User.create({
+        name, 
+        email, 
+        password: bcrypt.hashSync(password, bcryptSalt)
+    })
+
+    res.json(userDoc);
 })
 
 
--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -7,11 +7,15 @@ export default function RegiserPage() {
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    function registetUser(ev){
+    async function registetUser(ev) {
         ev.preventDefault();
-        axios.post('/register', {
+
+        await axios.post('/register', {
             name, email, password
         });
+
+
+
     }
 
     return (
@@ -19,15 +23,15 @@ export default function RegiserPage() {
             <div className="mb-64">
                 <h1 className="text-4xl text-center mb-4">Register</h1>
                 <form className="max-w-md mx-auto" onSubmit={registetUser}>
-                    <input type="text" placeholder="John Doe" 
-                    value={name} 
-                    onChange={ev => setName(ev.target.value)}/>
-                    <input type="email" placeholder="you@email.com" 
-                    value={email}
-                    onChange={ev => setEmail(ev.target.value)}/>
-                    <input type="password" placeholder="password" 
-                    value={password}
-                    onChange={ev => setPassword(ev.target.value)}/>
+                    <input type="text" placeholder="John Doe"
+                        value={name}
+                        onChange={ev => setName(ev.target.value)} />
+                    <input type="email" placeholder="you@email.com"
+                        value={email}
+                        onChange={ev => setEmail(ev.target.value)} />
+                    <input type="password" placeholder="password"
+                        value={password}
+                        onChange={ev => setPassword(ev.target.value)} />
                     <button className="primary">Register</button>
                     <div className="text-center text-gray-500">
                         If you already have an account <Link className="underline text-black" to={'/login'}>Login here</Link>
