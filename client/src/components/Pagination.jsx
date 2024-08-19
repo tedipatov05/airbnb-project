d6775bfc5057f9ce5614c7b4cf3c96cc53b8718b
Teodor <teodorpatov05@pmgkk.com>
2024-08-19 23:09:32 +0300

    Added pagination to my bookings

--- client/src/components/Pagination.jsx
+++ client/src/components/Pagination.jsx
@@ -1,18 +1,42 @@
-export default function Pagination() {
+
+export default function Pagination({ totalPlaces, placesPerPage, setCurrentPage, currentPage }) {
+
+    const pages = Math.ceil(totalPlaces / placesPerPage);
+    const pagesArr = [...Array(pages).keys()].map(i => i + 1);
+
+    function next(next) {
+        if (next > pages) {
+            setCurrentPage(pages)
+        } else {
+            setCurrentPage(next)
+        }
+
+    }
+
+    function prev(prev) {
+        if (prev < 1) {
+            setCurrentPage(1)
+        } else {
+            setCurrentPage(prev);
+        }
+    }
+
     return (
         <nav className="flex items-center gap-x-1 mt-8 mb-5  " aria-label="Pagination">
-            <button type="button" className="min-h-[38px] min-w-[38px] py-2 px-2.5 inline-flex justify-center text-base items-center gap-x-1.5 text-sm rounded-lg text-black hover:bg-gray-200 focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none " aria-label="Previous">
+            <button type="button" onClick={() => prev(currentPage - 1)} className="min-h-[38px] min-w-[38px] py-2 px-2.5 inline-flex justify-center text-base items-center gap-x-1.5 text-sm rounded-lg text-black hover:bg-gray-200 focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none " aria-label="Previous">
                 <svg aria-hidden="true" className="hidden shrink-0 size-3.5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                     <path d="m15 18-6-6 6-6"></path>
                 </svg>
                 <span>Previous</span>
             </button>
             <div className="flex items-center gap-x-1">
-                <button type="button" className="min-h-[38px] min-w-[38px] flex justify-center items-center text-base text-black hover:bg-gray-200 hover:text-gray-500 py-2 px-3 text-sm rounded-lg focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none" aria-current="page">1</button>
-                <button type="button" className="min-h-[38px] min-w-[38px] flex justify-center items-center text-base text-black hover:bg-gray-200 hover:text-gray-500 py-2 px-3 text-sm rounded-lg focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none ">2</button>
-                <button type="button" className="min-h-[38px] min-w-[38px] flex justify-center items-center text-base text-black hover:bg-gray-200 hover:text-gray-500 py-2 px-3 text-sm rounded-lg focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none ">3</button>
+                {pages > 0 && pagesArr.map((page, index) => (
+                    <button key={index} type="button" onClick={() => setCurrentPage(page)} className="min-h-[38px] min-w-[38px] flex justify-center items-center text-base text-black hover:bg-gray-200 hover:text-gray-500 py-2 px-3 text-sm rounded-lg focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none" aria-current="page">{page}</button>
+
+                ))}
+
             </div>
-            <button type="button" className="min-h-[38px] min-w-[38px] py-2 px-2.5 inline-flex justify-center text-base items-center gap-x-1.5 text-sm rounded-lg text-black hover:bg-gray-200 focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none" aria-label="Next">
+            <button type="button" onClick={() => next(currentPage + 1)} className="min-h-[38px] min-w-[38px] py-2 px-2.5 inline-flex justify-center text-base items-center gap-x-1.5 text-sm rounded-lg text-black hover:bg-gray-200 focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none" aria-label="Next">
                 <span>Next</span>
                 <svg aria-hidden="true" className="hidden shrink-0 size-3.5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                     <path d="m9 18 6-6-6-6"></path>
--- client/src/pages/BookingsPage.jsx
+++ client/src/pages/BookingsPage.jsx
@@ -2,66 +2,39 @@ import { useDebugValue, useEffect, useState } from "react";
 import AccountNav from "../components/AccountNav";
 import axios from 'axios';
 import PlaceImg from "../components/PlaceImg";
-import { differenceInCalendarDays, format } from "date-fns";
-import { Link } from "react-router-dom";
-import BookingDates from "../components/BookingDates";
+import { Link, useParams } from "react-router-dom";
+import Bookings from "../components/Bookings";
 import Pagination from "../components/Pagination";
 
+
 export default function BookingsPage() {
+    const [currentPage, setCurrentPage] = useState(1);
+    const [placesPerPage, setPlacesPerPage] = useState(8);
 
     const [bookings, setBookings] = useState([]);
 
     useEffect(() => {
         axios.get('/bookings').then(response => {
-            setBookings([...response.data, ...response.data, ...response.data]);
+
+            setBookings([...response.data, ...response.data, ...response.data, ...response.data, ...response.data, ...response.data, ...response.data]);
         })
     }, []);
 
+    const lastPlaceIndex = currentPage * placesPerPage;
+    const firstPlaceIndex = lastPlaceIndex - placesPerPage;
+    const currentBookings = bookings.slice(firstPlaceIndex, lastPlaceIndex);
+
+    
 
     return (
         <div >
             <AccountNav />
             <div className="grid grid-rows-2" style={{ 'gridTemplateColumns': "repeat(4, minmax(0, 0.25fr))" }}>
-                {/* maps all bookings */}
-                {bookings?.length > 0 && bookings.map(booking => (
-                    <div className="block rounded-lg bg-gray-100 w-72 mt-10" key={booking.place.title}>
-                        <div className="relative overflow-hidden bg-cover bg-no-repeat" >
-                            <PlaceImg place={booking.place} className={"rounded-lg  sm:m-h-64 md:h-64 w-full"} />
-                            <Link to={`/account/bookings/${booking._id}`}>
-                                <div
-                                    className="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-[hsla(0,0%,98%,0.15)] bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-100">
-                                </div>
-                            </Link>
-                        </div>
-
-                        <div className="p-2">
-                            <div className="flex justify-between">
-                                <h5 className="mb-2 text-base font-bold leading-tight ">
-                                    {booking.place.title}
-                                </h5>
-                            </div>
-                            <p className="mb-1 text-sm flex gap-1">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z" />
-                                </svg>
-
-                                <span className="text-lg">{differenceInCalendarDays(new Date(booking.checkOut), new Date(booking.checkIn))} nights</span>
-                            </p>
-
-                            <h5 className="mb-2 text-lg font-bold leading-tight ">
-                                Total price: ${booking.price}
-                            </h5>
-                        </div>
-                    </div>
-                ))}
-
-
-
+                <Bookings bookings={currentBookings} />
 
             </div>
-            <div className="flex justify-center">
-                <Pagination />
-
+            <div className="pagination">
+                <Pagination totalPlaces={bookings.length} placesPerPage={placesPerPage} setCurrentPage={setCurrentPage} currentPage={currentPage} />
             </div>
         </div>
     )
